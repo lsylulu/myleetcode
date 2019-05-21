2514086c57f611ca60a56c25815a17534d45cb2f
shengye.lv <739352820@qq.com>
2019-05-21 19:13:23 +0800

    50题-- 计算几的几次幂

--- src/solution5/PowxN.java
+++ src/solution5/PowxN.java
@@ -30,7 +30,7 @@ public class PowxN {
      * @param n
      * @return
      */
-    public double myPow(double x, int n) {
+    public static double myPow(double x, int n) {
         double res = 1.0;
 
         for(int i = n; i != 0; i /= 2){
@@ -40,5 +40,9 @@ public class PowxN {
             x *= x;
         }
         return  n < 0 ? 1 / res : res;
+    }
+
+    public static void main(String[] args) {
+        myPow(2.00000, 10);
     }
 }
