ee44039730e8fb3a63cf7c6fb8af70b51803faf8
shengye.lv <739352820@qq.com>
2019-07-07 21:42:04 +0800

    剑指offer刷题

--- src/swordfingeroffer/ReplaceBlankSpace.java
+++ src/swordfingeroffer/ReplaceBlankSpace.java
@@ -5,4 +5,23 @@ package swordfingeroffer;
  * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
  */
 public class ReplaceBlankSpace {
+
+    /**
+     * 采用字符数组进行巧妙的替换
+     * @param str
+     * @return
+     */
+    public String replaceSpace(StringBuffer str) {
+        String s=str.toString();
+        char[] ch=s.toCharArray();
+        StringBuffer res=new StringBuffer();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]==' '){
+                res.append("%20");
+            }else{
+                res.append(ch[i]);
+            }
+        }
+        return res.toString();
+    }
 }
