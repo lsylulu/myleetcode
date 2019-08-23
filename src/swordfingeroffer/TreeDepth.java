6b9d94a3b2e03d80fc829fd0395328e9fbbea3f9
shengye.lv <739352820@qq.com>
2019-08-23 22:21:26 +0800

    剑指offer刷题

---
+++ src/swordfingeroffer/TreeDepth.java
@@ -0,0 +1,22 @@
+package swordfingeroffer;
+
+/**
+ * 输入一棵二叉树，求该树的深度。
+ * 从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+public class TreeDepth {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int TreeDepth(TreeNode root) {
+        return root==null?0:Math.max(TreeDepth(root.left),TreeDepth(root.right))+1;
+    }
+}
